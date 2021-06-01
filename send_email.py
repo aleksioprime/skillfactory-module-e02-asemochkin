e5c33847bd18d9839ca5b2a156e46fc352c606c0
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-06-01 18:36:32 +0300

    edit

--- send_email.py
+++ send_email.py
@@ -4,13 +4,9 @@ from sendgrid.helpers.mail import Mail
 
 
 def send_email_with_text(text, email_from, email_to):
-    message = Mail(
-        from_email=email_from,
-        to_emails=email_to)
+    message = Mail(from_email=email_from, to_emails=email_to)
     message.template_id = os.environ.get('TEMPLATE_ID')
-    message.dynamic_template_data = {
-            "text": text,
-        }
+    message.dynamic_template_data = {"text": text}
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API'))
         response = sg.send(message)
